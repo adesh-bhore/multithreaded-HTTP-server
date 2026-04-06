4f6799b92187652e11f1a15cd4d617baa89668fd
adesh-bhore <adeshbhore107@gmail.com>
2026-04-06 12:02:25 +0530

    inital commit

--- server_globals.h
+++ server_globals.h
@@ -13,6 +13,11 @@ static inline void signal_main_loop(void)
 {
     uint64_t val = 1;
     if (write(g_eventfd, &val, sizeof(val)) < 0 && errno != EAGAIN)
+    {
+        // In a real server, we might want to handle this more gracefully.
+        // For this simple example, we'll just print an error and continue.
+        printf("eventfd write");
+    }
         // perror("eventfd write");
 }
 
