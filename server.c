7a2618b40ea6a20a769902c17729a75ab4ba4e89
adesh-bhore <adeshbhore107@gmail.com>
2026-04-06 12:04:06 +0530

    inital commit

--- server.c
+++ server.c
@@ -321,23 +321,6 @@ done:
 }
 }
 
-void conn_destroy(connection_t *conn, int epoll_fd)
-{
-    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, conn->fd, NULL);
-    close(conn->fd);
-
-    if (conn->timer_fd > 0)
-    {
-        epoll_ctl(epoll_fd, EPOLL_CTL_DEL, conn->timer_fd, NULL);
-        close(conn->timer_fd);
-    }
-
-    if (conn->file_fd > 0)
-        close(conn->file_fd);
-
-    free(conn);
-}
-
 
 int main(void)
 {
